33c5772c725d53258c02c868bc895818b979d699
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-02-14 12:28:11 -0800

    CSS styling on show

--- frontend/components/tree_house_show/tree_house_show.jsx
+++ frontend/components/tree_house_show/tree_house_show.jsx
@@ -38,7 +38,6 @@ class TreeHouseShow extends React.Component {
     if (currentTreeHouse && usersHaveLoaded) {
       const reviews = this.props.treeHouses.currentTreeHouse.reviews;
       const bookings = this.props.treeHouses.currentTreeHouse.bookings;
-      //use state?
       const host = this.state.host;
 
       const backgroundImageStyles = {
