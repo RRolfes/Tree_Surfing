7fc8c896b0f96a21d1ca3835e09e34ee27889431
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-02-27 08:48:12 -0800

    Adds booking form note

--- frontend/components/tree_house_show/tree_house_show.jsx
+++ frontend/components/tree_house_show/tree_house_show.jsx
@@ -50,7 +50,7 @@ class TreeHouseShow extends React.Component {
             <div className='tree-house-detail-photo' style={backgroundImageStyles}></div>
           </div>
           // break out review items
-          // add widget for booking
+          // add widget for booking form
           <div className='tree-house-detail-container'>
             <div className="tree-house-centered">
               <div className="tree-house-content">
