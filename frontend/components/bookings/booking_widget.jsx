11161bb2721d3b503fd13734e92fa9546de6e83a
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-03-02 15:09:29 -0800

    Adds price and styling to booking widget

--- frontend/components/bookings/booking_widget.jsx
+++ frontend/components/bookings/booking_widget.jsx
@@ -18,6 +18,10 @@ class BookingWidget extends React.Component {
     window.addEventListener('scroll', this.handleScroll);
   }
 
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   handleScroll(e) {
     if (!this.state.fixed && window.scrollY >= 450) {
       this.setState({fixed: true});
@@ -27,12 +31,15 @@ class BookingWidget extends React.Component {
   }
 
   render() {
-
+    const treeHouse = this.props.treeHouse;
     let fixed = this.state.fixed ? ("widget-fixed") : ("");
 
     return(
       <div className={`booking-widget ${fixed}`}>
-        <div>TEXT TEXT TEXT TEXT TEXT TEXT</div>
+        <div className="price-bar">
+          <span className="price-text">${treeHouse.price}</span>
+          <span className="per-night-text"> per night</span>
+        </div>
       </div>
     );
   }
