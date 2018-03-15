40326daba612f9e9452e2089ef2b655207cd942a
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-03-15 14:43:48 -0700

    Creates form modal and removes sign up modal

--- frontend/components/modals/form_modal.jsx
+++ frontend/components/modals/form_modal.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import LogInFormContainer from '../log_in/log_in_form_container';
-
 
 class LogInModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open : false,
-      current : "Sign Up"
     };
 
     this.closeModal = this.closeModal.bind(this);
@@ -30,23 +27,15 @@ class LogInModal extends React.Component {
   }
 
   render() {
-
+    let buttonName = this.props.buttonName;
     let open = this.state.open ? ("modal-open") : ("");
-    let current = (this.state.current !== '' && this.state.current === "Sign Up") ? true : false;
-
-
-    debugger;
 
     return(
 
       <section>
-        <input className="modal-input" type="button" value="Log In" onClick={ () => this.openModal()}/>
+        <input className="modal-input" type="button" value={buttonName} onClick={ () => this.openModal()}/>
         <div className={`modal-container ${open}`}>
           <div className="modal-contents">
-            <div className="modal-and-switch">
-              <LogInFormContainer />
-              <input className="switch" type="button" value="opposite" />
-            </div>
 
           </div>
         </div>
