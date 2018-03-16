7d0ca76277ea12506c0b3c674f9753ffa41d8719
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-03-15 17:31:59 -0700

    Begins Reviews refactor

---
+++ frontend/components/reviews/review_index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReviewItem from './review_item';
+
+class ReviewsIndex extends React.Component {
+  render() {
+    const reviews = this.props.reviews;
+
+    return(
+      <div className="review-index-container">
+        
+      </div>
+    );
+  }
+}
+
+export default ReviewsIndex;
