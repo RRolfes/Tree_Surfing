bfb75f957b304e4c7b1ad1abf8e15e042ec50a34
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-03-16 10:15:29 -0700

    Adds event listener for Best and Recent comments exit

--- frontend/components/reviews/reviews_index.jsx
+++ frontend/components/reviews/reviews_index.jsx
@@ -4,12 +4,25 @@ import ReviewItem from './review_item';
 class ReviewsIndex extends React.Component {
   constructor(props) {
     super(props);
+      this.state = {
+        order: "recent",
+        reviews: ['a', 'b', 'c']
+      };
 
+    this.handleOrder = this.handleOrder.bind(this);
   }
+
+  handleOrder(e) {
+    if (e.target.value === "Best") {
+      this.setState( {order: "Best" });
+    } else if (e.target.value === "Recent") {
+      this.setState( {order: "Recent" });
+    }
+  }
+
 
   render() {
     const reviews = this.props.reviews.reverse();
-    console.log(reviews);
 
     return(
       <div className="review-index-container">
@@ -18,9 +31,9 @@ class ReviewsIndex extends React.Component {
             {reviews.length} Reviews
           </div>
           <div className="reviews-best-recent">
-            <div className="reviews-best">Best</div>
+            <input className="reviews-best" type="button" onClick={this.handleOrder} value="Best"/>
             /
-            <div className="reviews-recent">Recent</div>
+            <input className="reviews-recent" type="button" onClick={this.handleOrder} value="Recent"/ >
           </div>
         </div>
         {reviews.map( review => (
